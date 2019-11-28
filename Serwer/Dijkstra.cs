d68bce99bed62a3324893b7fb8f250412bebc1f5
sdziura <sabek12@gmail.com>
2019-11-28 22:05:00 +0100

    Dodaj pliki projektów.

---
+++ Serwer/Dijkstra.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+
+    class Dijkstra
+    {
+        Graph graph { get; set; }
+        // Table informing if a node is already included in path
+        bool[] onPath { get; set; } 
+        // Current shortest known distance from start node to node of index
+        int[] shortestDist { get; set; }
+        // From which node we got to the considered node
+        int[] previous { get; set; }
+
+        public Dijkstra(Graph _graph)
+        {
+            graph = _graph;
+            onPath = new bool[graph.nrOfNodes];
+            shortestDist = new int[graph.nrOfNodes];
+            previous = new int[graph.nrOfNodes];
+            for (int i = 0; i < graph.nrOfNodes; ++i)
+            {
+                onPath[i] = false;
+                shortestDist[i] = int.MaxValue;
+                previous[i] = 0;
+            }
+            onPath[graph.start] = true;
+        }
+
+        public void sprawdz()
+        {
+            for(int i = 0; i < graph.nrOfNodes; ++i)
+            {
+                Console.WriteLine(onPath[i] + "\t" + shortestDist[i] + "\t" + previous[i] + "\n");
+            }
+        }
+    }
+}
---
+++ Serwer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] heh = new int[,] { { 0, 2, 3, 4 }, { 2, 0, 1, 2 }, { 3, 1, 0, 2 }, { 4, 2, 2, 0 } };
+            Graph graf = new Graph(4, heh, 1, 3);
+            Dijkstra dijk = new Dijkstra(graf);
+            dijk.sprawdz();
+            Console.ReadKey();
+        }
+    }
+}
