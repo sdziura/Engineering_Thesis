895cbaed85f21994acb4411a4c4a3107cf58783c
sdziura <sabek12@gmail.com>
2019-12-24 00:24:31 +0100

    Ready basic Dijsktra algorithm

--- Serwer/Dijkstra.cs
+++ Serwer/Dijkstra.cs
@@ -15,28 +15,75 @@ namespace Server
         // Current shortest known distance from start node to node of index
         int[] shortestDist { get; set; }
         // From which node we got to the considered node
-        int[] previous { get; set; }
+        int[] predecessor { get; set; }
+        // Furthest node we got to 
+        int current; 
 
         public Dijkstra(Graph _graph)
         {
             graph = _graph;
             onPath = new bool[graph.nrOfNodes];
             shortestDist = new int[graph.nrOfNodes];
-            previous = new int[graph.nrOfNodes];
+            predecessor = new int[graph.nrOfNodes];
+            current = graph.start;
             for (int i = 0; i < graph.nrOfNodes; ++i)
             {
                 onPath[i] = false;
                 shortestDist[i] = int.MaxValue;
-                previous[i] = 0;
+                predecessor[i] = -1;
             }
-            onPath[graph.start] = true;
+            onPath[graph.start] = true;   // Set starting node as visited
+            shortestDist[graph.start] = 0;  // 0 weight to starting node
+        }
+  
+        private int findShortestWay()
+        {
+            int shortest = int.MaxValue;
+            int pathCandidate = -1;
+            for (int j = 0; j < graph.nrOfNodes; ++j)
+            {
+                if (!onPath[j])
+                {
+                    if (shortestDist[current] + graph.graph[current, j] < shortestDist[j])
+                    {
+                        shortestDist[j] = shortestDist[current] + graph.graph[current, j];
+                        predecessor[j] = current;
+                    }
+                    if (shortestDist[j] < shortest)
+                    {
+                        shortest = shortestDist[j];
+                        pathCandidate = j;
+                    }
+                }
+            }
+            current = pathCandidate;
+            onPath[pathCandidate] = true;
+            return pathCandidate;
+        }
+
+        public void alghoStart()
+        {
+            while(current!=graph.end)
+                findShortestWay();   
+        }
+
+        public List<int> showWay()
+        {
+            int temp = graph.end;
+            List<int> way = new List<int>();
+            way.Add(temp);
+            while(temp != graph.start)
+                 way.Add(temp = predecessor[temp]);
+
+            way.Reverse();
+            return way;
         }
 
         public void sprawdz()
         {
             for(int i = 0; i < graph.nrOfNodes; ++i)
             {
-                Console.WriteLine(onPath[i] + "\t" + shortestDist[i] + "\t" + previous[i] + "\n");
+                Console.WriteLine(onPath[i] + "\t" + shortestDist[i] + "\t" + predecessor[i] + "\n");
             }
         }
     }
--- Serwer/Program.cs
+++ Serwer/Program.cs
@@ -14,6 +14,12 @@ namespace Server
             Graph graf = new Graph(4, heh, 1, 3);
             Dijkstra dijk = new Dijkstra(graf);
             dijk.sprawdz();
+            dijk.alghoStart();
+            foreach (int i in dijk.showWay())
+            {
+                Console.Write("{0}\t", i.ToString());
+            }
+            dijk.sprawdz();
             Console.ReadKey();
         }
     }
